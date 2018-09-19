222c8d0fe3126715f0a444ea7bc332fde1855b2e
Fish-bowl <symbolt.ap@gmail.com>
2018-09-18 19:16:31 -0600

    added notes for TA's

--- src/components/Fridge.js
+++ src/components/Fridge.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { connect } from "react-redux";
 import Note from './Note'
 
+//are all my notes params going in right here 
+//-----------------|*|
 const filtered = (notes, filter) => {
   switch (filter) {
     case 'ALL':
@@ -11,7 +13,16 @@ const filtered = (notes, filter) => {
     case 'Completed':
       return notes.filter( t => t.complete )
     case 'Red':
-      return notes.filter( t => t.red)
+      //what is t and what defines it. how do I not use t and use notes params?????????
+      //the previous ( t => t.complete ) makes sense because it's a boolean, and I'm checking that. 
+      //if the params are going in then shouldnt I be able to check against this string? 
+      //when I look at redux tools. it says its looking only for whether or not its completed. how would I change 
+      //btw when I click the different buttons before I've added a note, redux tools says it's looking for the button i clicked before the current button......funky logic error?
+      return notes.filter( t => this.color == 'red' )
+    case 'Blue':
+      return notes.filter( t => this.color == 'blue' )
+    case 'Green':
+      return notes.filter( t => this.color == 'green' )
   }
 }
 
